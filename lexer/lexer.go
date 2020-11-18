14a5b3a1907ca3ea9fd489acbbe5889ab414fdcb
Chris Wendt <chrismwendt@gmail.com>
2020-11-18 13:23:31 -0700

    Skip whitespace

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -28,6 +28,8 @@ func (l *Lexer) readChar() {
 func (l *Lexer) NextToken() token.Token {
 	var tok token.Token
 
+	l.skipWhitespace()
+
 	switch l.ch {
 	case '=':
 		tok = newToken(token.ASSIGN, l.ch)
@@ -60,6 +62,12 @@ func (l *Lexer) NextToken() token.Token {
 
 	l.readChar()
 	return tok
+}
+
+func (l *Lexer) skipWhitespace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
 }
 
 func (l *Lexer) readIdentifier() string {
